3e003bf8011846bf77c49c862ee4733d5093e688
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 02:09:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| "#{call.capitalize}!" }
 end
 
-def long_planeteer_calls()
-  # Your code here
+def long_planeteer_calls(long_calls)
+  # Returns true if any call is longer than 4 characters.
+  
+  long_calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
